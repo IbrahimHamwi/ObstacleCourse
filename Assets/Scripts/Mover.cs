ab3bd67385b8e4557ec7a182332b80d4aef9297d
IbrahimHamwi <ibrahimhamwi_619@hotmail.com>
2021-11-26 01:36:57 +0300

    Awaiting Prefabs

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,14 +8,25 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0, zValue);
     }
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move the player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+
 }
